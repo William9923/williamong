9a91026d62d4a88d5626c7fc9a19dfa631bace32
William9923 <williamong1400@gmail.com>
2022-03-04 18:35:55 +0700

    feat: add adjustment for navigation bar

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <div className="flex justify-center items-centers">
-        <div className="w-3/4">
+        <div className="w-5/6 md:w-3/4">
         <Component {...pageProps} />
       </div>
     </div>
