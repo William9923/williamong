2e54c99882d134e9cfee7d58b828d64ece56113e
William9923 <williamong1400@gmail.com>
2022-03-11 11:05:19 +0700

    deploy: fix deployment

--- next.config.js
+++ next.config.js
@@ -1,61 +1,16 @@
-const path = require('path');
-
-const withImages = require('next-images')
-const debug = process.env.NODE_ENV !== "production";
-module.exports = withImages({
-    webpack(
-        config,
-        {
-            dev = process.env.NODE_ENV === 'development',
-            isServer = typeof window === 'undefined'
-        }
-    ) {
-        if (isServer) {
-            require('./scripts/generate-sitemap');
-            console.log("Generating sitemap...")
-        }
-        /**
-         * !dev ? preact/compat : react, react-dom on build
-         * reduce page weight in production by ~10%
-         */
-        return config;
-    },
-    resolve: {
-        fallback: {
-            "fs": false
-        },
-    },
-    sourceMaps: {
-        productionBrowserSourceMaps: true
-    },
+module.exports = {
     images: {
-        domains: [
-            'avatars.githubusercontent.com',
-            'faderoom-headless.us',
-            'www.faderoom-headless.us',
-            'dtmqnbkq3btfh.cloudfront.net',
-            'secure.gravatar.com',
-            'automattic.com',
-            'serve.onegraph.com',
-            'onegraph.com',
-            'maps.google.com',
-            'lh3.googleusercontent.com',
-            'maps.gstatic.com',
-            'thefaderoom146.booksy.com',
-            'dev-3cqt2bq0.auth0.com',
-            'scontent-sea1-1.xx.fbcdn.net',
-            'd2zdpiztbgorvt.cloudfront.net',
-            'platform-lookaside.fbsbx.com',
-            'square-postoffice-production.s3.amazonaws.com'
-        ]
+      domains: [
+        "i.scdn.co", // Spotify Album Art
+        "pbs.twimg.com", // Twitter Profile Picture
+        "api.microlink.io", // Microlink Image Preview
+      ],
     },
-    future: {
-        webpack5: true,
-        strictPostcssConfiguration: true
+    webpack: (config, { dev, isServer }) => {
+      if (isServer) {
+        require("./scripts/generate-sitemap");
+      }
+  
+      return config;
     },
-    i18n: {
-        locales: ['en-US'],
-        defaultLocale: 'en-US'
-    },
-    assetPrefix: !debug ? 'https://williamong.vercel.app/' : '',
-});
+  };
