b2636f40d5d88bb2e188d07a63d312f0ed0760d8
William9923 <williamong1400@gmail.com>
2022-06-30 13:50:11 +0700

    feat: add carousel effect

--- components/Projects.js
+++ components/Projects.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiGithub, FiTwitter, FiSlack, FiLinkedin } from "react-icons/fi";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+/**
+ * Utility to detect if you're on the server, or in the browser.
+ */
+const isBrowser = typeof window !== "undefined";
 
 import PageTitle from "@components/PageTitle";
 
@@ -35,7 +42,6 @@ const featuredProjects = [
 
 const images = ['https://images.unsplash.com/photo-1506501139174-099022df5260?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80', 'https://images.unsplash.com/photo-1523438097201-512ae7d59c44?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80', 'https://images.unsplash.com/photo-1513026705753-bc3fffca8bf4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80']
 
-// TODO: Handle Featured Projects & Only show featured project on small device...
 const SmallCard = ({ title, desc, link, imgUrl, tags }) => {
   return (
     <a href={link} target="_blank">
@@ -48,9 +54,9 @@ const SmallCard = ({ title, desc, link, imgUrl, tags }) => {
                 src={imgUrl}
                 alt={title}
               />
-              <div className="absolute hidden group-hover:block bottom-0 right-0 bg-blue-200 text-sm text-black hover:text-white p-2 rounded-full hover:bg-blue-500 m-2">
-                <FiGithub size={25} />
-              </div>
+              <button className="absolute hidden group-hover:block mr-4 top-0 mt-32 right-0 bg-white rounded-full shadow-md h-12 w-12 text-2xl text-indigo-600 hover:text-indigo-400 focus:text-indigo-400 focus:outline-none focus:shadow-outline">
+                <span className="block">➜</span>
+              </button>
             </div>
           )}
 
@@ -74,94 +80,44 @@ const SmallCard = ({ title, desc, link, imgUrl, tags }) => {
   )
 }
 
+const openInNewTab = (url) => {
+  const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+  if (newWindow) newWindow.opener = null
+}
 
-// TODO: Adjust with this..https://tailwind-elements.com/docs/standard/components/carousel/
-const Carousel = () => {
-  // We will start by storing the index of the current image in the state.
-  const [currentImage, setCurrentImage] = React.useState(0);
-
-  // We are using react ref to 'tag' each of the images. Below will create an array of
-  // objects with numbered keys. We will use those numbers (i) later to access a ref of a
-  // specific image in this array.
-  const refs = images.reduce((acc, val, i) => {
-    acc[i] = React.createRef();
-    return acc;
-  }, {});
-
-  const scrollToImage = (i) => {
-    // First let's set the index of the image we want to see next
-    setCurrentImage(i);
-    // Now, this is where the magic happens. We 'tagged' each one of the images with a ref,
-    // we can then use built-in scrollIntoView API to do eaxactly what it says on the box - scroll it into
-    // your current view! To do so we pass an index of the image, which is then use to identify our current
-    // image's ref in 'refs' array above.
-    refs[i].current.scrollIntoView({
-      //     Defines the transition animation.
-      behavior: "smooth",
-      //      Defines vertical alignment.
-      block: "nearest",
-      //      Defines horizontal alignment.
-      inline: "start",
-    });
-  };
-
-  // Some validation for checking the array length could be added if needed
-  const totalImages = images.length;
-
-  // Below functions will assure that after last image we'll scroll back to the start,
-  // or another way round - first to last in previousImage method.
-  const nextImage = () => {
-    if (currentImage >= totalImages - 1) {
-      scrollToImage(0);
-    } else {
-      scrollToImage(currentImage + 1);
-    }
-  };
-
-  const previousImage = () => {
-    if (currentImage === 0) {
-      scrollToImage(totalImages - 1);
-    } else {
-      scrollToImage(currentImage - 1);
-    }
-  };
-
-  // Tailwind styles. Most importantly notice position absolute, this will sit relative to the carousel's outer div.
-  const arrowStyle =
-    "absolute text-white text-2xl z-10 bg-black h-10 w-10 rounded-full opacity-75 flex items-center justify-center";
-
-  // Let's create dynamic buttons. It can be either left or right. Using
-  // isLeft boolean we can determine which side we'll be rendering our button
-  // as well as change its position and content.
-  const sliderControl = (isLeft) => (
-    <button
-      type="button"
-      onClick={isLeft ? previousImage : nextImage}
-      className={`${arrowStyle} ${isLeft ? "left-2" : "right-2"}`}
-      style={{ top: "40%" }}
-    >
-      <span role="img" aria-label={`Arrow ${isLeft ? "left" : "right"}`}>
-        {isLeft ? "◀" : "▶"}
-      </span>
-    </button>
-  );
+const ProjectList = ({ projects }) => {
 
   return (
-    <div className="w-screen flex justify-center">
-      <div className="p-12 flex justify-center w-screen md:w-1/2 items-center">
-        <div className="relative w-full">
-          <div className="carousel">
-            {sliderControl(true)}
-            {images.map((img, i) => (
-              <div className="w-full flex-shrink-0" key={img} ref={refs[i]}>
-                <img src={img} className="w-full object-contain" />
+    <center>
+      <Carousel showThumbs={false} showStatus={false} emulateTouch={true} width={"90%"}>
+        {projects.map((proj) => (
+          <>
+            <img className="rounded-xl object-scale-down h-96 w-48" src={proj.imgUrl} />
+            <div className="legend">
+              <div className="flex flex-col justify-between px-2 sm:pb-2 block group-hover:hidden">
+                <div className="mb-2">
+                  <div className="text-white font-bold text-xl mb-2">{proj.title}</div>
+                  <p className="text-grey-darker text-base">{proj.desc}</p>
+                </div>
+                <div className="pt-2 pb-1">
+                  {(proj.tags != undefined && proj.tags.length > 0) && proj.tags.map((tag, idx) => (
+                    <button key={idx} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:scale-110">#{tag}</button>
+                  ))}
+                </div>
+                <a href={proj.url} target="_blank">
+                  <button className="absolute mr-8 top-0 mt-28 right-0 bg-white rounded-full shadow-md h-12 w-12 text-2xl text-indigo-600 hover:text-indigo-400 focus:text-indigo-400 focus:outline-none focus:shadow-outline">
+                    <span className="block">➜</span>
+                  </button>
+                </a>
+
               </div>
-            ))}
-            {sliderControl()}
-          </div>
-        </div>
-      </div>
-    </div>
+
+            </div>
+          </>
+        ))}
+      </Carousel>
+    </center>
+
   );
 };
 
@@ -171,13 +127,14 @@ export default function Projects({ data }) {
 
   return (
     <div className="bg-white flex flex-col justify-center" id="projects">
-      {/* Carousel for projects... */}
       <div className="max-w-6xl mx-auto bg-white">
         <PageTitle title={title} quotes={quotes} />
       </div>
-      <Carousel />
-      {/* Featured Projects */}
-      <SectionTitle title={"Featured Projects"} />
+      <ProjectList projects={projects} />
+      <center>
+        <SectionTitle title={"Featured Projects"} />
+      </center>
+
       <div className="flex flex-col md:flex-row justify-between py-5 pb-10">
         {
           featuredProjects.map((proj) => (
